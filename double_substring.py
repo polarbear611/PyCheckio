f9fa1ea34219c05ce9add01e3bbf80a8ef51b428
polarbear611 <huangqin611@gmail.com>
2017-11-30 00:21:22 +0800

    deleted:    double_substring.py

--- double_substring.py
+++
@@ -1,9 +0,0 @@
-def repeat_len(text):
-	
-
-def checkio(text):
-	max_len = 1
-	for i in range(len(text)):
-		for j in range(i + max_len, len(text), 1):
-			rl = repeat_len(text[i:j])
-				max_len = max(rl, max_len)
